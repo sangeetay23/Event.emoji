1b279b777ba833ac0497f686e40185b3eab28fa0
SANGEETA YADAV <sangeetayadav23@navgurukul.org>
2024-06-28 12:06:25 +0530

    Add files via upload

---
+++ eventemoji.js
@@ -0,0 +1,6 @@
+function changeHeading() {
+    var img = document.getElementById("Nav");
+    img.src = "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/65532/happy-emoji-clipart-md.png";
+    img.style.height = "500px";
+    img.style.width = "500px";
+}
